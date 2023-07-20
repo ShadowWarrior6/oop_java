ac134c7e991276683aef64a8cbafa23cec753c8f
Andranik <andran1401@gmail.com>
2023-07-20 12:19:07 +0400

    cat

---
+++ homework/task01.java
@@ -0,0 +1,24 @@
+package homework;
+
+public class task01{
+ public static void main(String[] args){
+        cat cat=new cat("Семён",5);
+        System.out.println("Мяу! Меня зовут:" +  cat.getName()+ "  мне" +cat.getAge() + "  лет");
+    }
+}
+class cat{
+    private String name ;
+    private int age ;
+    public cat(String name,int age){
+    this.name=name;
+    this.age=age;
+
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+
+    }
+}
