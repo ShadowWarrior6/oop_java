a4fb0af3ec4e0235a6cdb3d5d98c23019dee2c55
Andranik <andran1401@gmail.com>
2023-07-20 18:39:29 +0400

    final

--- homework/task01.java
+++ homework/task01.java
@@ -3,17 +3,21 @@ package homework;
 public class task01{
  public static void main(String[] args){
         cat cat=new cat("Семён",5);
-        System.out.println("Мяу! Меня зовут:" +  cat.getName()+ "  мне" +cat.getAge() + "  лет");
+        Owner owner=new Owner("Джон");
+        
+        System.out.println("Мяу! Меня зовут:" +  cat.getName()+ "  мне " +cat.getAge() + "  лет моего хозяиня зовут " + owner.getName());
     }
 }
-class cat{
+class cat {
     private String name ;
     private int age ;
     public cat(String name,int age){
     this.name=name;
     this.age=age;
+    
 
     }
+   
     public String getName(){
         return name;
     }
@@ -22,6 +26,31 @@ class cat{
 
     }
     public void greet () {
-    System.out.printf("Мяу! Меня зовут %s. Мне %s года(лет).", name, age);
+    System.out.printf("Мяу! Меня зовут %s. Мне %s года(лет) мой мой хозяин %s.", name, age);
   }
+  
 }
+// class Owner extends task01{
+//     String name;
+//    public void Owner (String name){
+//     this.name=name;
+//    }
+//    public String getName(){
+//     return name;
+//    }
+
+   
+
+class Owner {
+   public String name; // Приватное поле name
+    
+
+    public Owner(String name) {
+        this.name = name;
+        
+    }
+    public String getName(){
+        return name;
+    }
+}
+
